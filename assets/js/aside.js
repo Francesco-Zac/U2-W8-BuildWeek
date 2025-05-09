268f8b86552bbee9b857cecc58b91f2c34541e0c
GianlucaFiorani <gianluca.fiorani97@gmail.com>
2025-05-09 12:12:22 +0200

    add responsivness

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -2,7 +2,10 @@ const pause = document.querySelector(".bi-pause-circle-fill");
 const play = document.querySelector(".bi-play-circle-fill");
 const playBtn = document.getElementById("play-btn");
 const heartBtn = document.getElementById("heart-btn");
-const heart = document.querySelector(".bi-heart-fill");
+const playTitle = document.getElementById("play-title");
+const playArttist = document.getElementById("play-artist");
+const playImg = document.getElementById("play-img");
+const heartBtnFull = document.getElementById("heart-btn-full");
 const svg = document.querySelectorAll(".navbar svg");
 const volumeIcone = document.getElementById("volume-icon");
 const volumeMute = document.getElementById("volume-mute");
@@ -17,34 +20,154 @@ const leftOpen = document.getElementById("left-open");
 const right = document.getElementById("right");
 const center = document.getElementById("center");
 const notify = document.getElementById("notify");
+const notiBage = document.getElementById("notify-bage");
+const frends = document.getElementById("frend");
+const logoClose = document.getElementById("logo-closening");
+const logoOpen = document.getElementById("logo-opener");
+const logoClose2 = document.getElementById("logo-closening2");
+const logoOpen2 = document.getElementById("logo-opener2");
+const btnSide = document.getElementById("logo-close-btn");
+const btnSide2 = document.getElementById("logo-close-btn2");
+const leftClose = document.getElementById("left-close");
+const leftImgList = document.getElementById("left-img-list");
+
+const addImgLeft = function () {
+  const library = document.querySelectorAll(".library-card");
+  library.forEach((card) => {
+    const libTitle = card.querySelector(".lib-title").innerText;
+    if (libTitle === playTitle.innerText) {
+      card.remove();
+      localStorage.setItem("leftCol", playlist.innerHTML);
+    }
+  });
+};
+
+const favoriteControl = function () {
+  const library = document.querySelectorAll(".library-card");
+  if (library.length > 0) {
+    library.forEach((card) => {
+      const libTitle = card.querySelector(".lib-title").innerText;
+      if (libTitle === playTitle.innerText) {
+        heartBtn.classList.add("d-none");
+        heartBtnFull.classList.remove("d-none");
+      } else {
+        heartBtnFull.classList.add("d-none");
+        heartBtn.classList.remove("d-none");
+      }
+    });
+  } else {
+    heartBtnFull.classList.add("d-none");
+    heartBtn.classList.remove("d-none");
+  }
+};
+
+btnSide.onmouseover = function () {
+  if (window.innerWidth > 992) {
+    logoClose.classList.add("d-none");
+    logoOpen.classList.remove("d-none");
+  }
+};
+btnSide.onmouseout = function () {
+  if (window.innerWidth > 992) {
+    logoClose.classList.remove("d-none");
+    logoOpen.classList.add("d-none");
+  }
+};
+btnSide2.onmouseover = function () {
+  logoClose2.classList.add("d-none");
+  logoOpen2.classList.remove("d-none");
+};
+btnSide2.onmouseout = function () {
+  logoClose2.classList.remove("d-none");
+  logoOpen2.classList.add("d-none");
+};
 
 heartBtn.onclick = function () {
-  heart.classList.toggle("opacity-0");
+  heartBtn.classList.add("d-none");
+  heartBtnFull.classList.remove("d-none");
+  const img = playImg.src;
+  const title = playTitle.innerText;
+  const album = playArttist.innerText;
+  const playData = {
+    album: { cover_small: img, titele: album },
+    title: title,
+  };
+  cardGen(true, playData);
+  const sideCloseImg = document.createElement("img");
+  sideCloseImg.classList.add("side-img", "rounded-circle");
+  sideCloseImg.src = playImg.src;
+  sideCloseImg.style.width = "48px";
+  leftImgList.appendChild(sideCloseImg);
+
+  localStorage.setItem("leftCol", playlist.innerHTML);
+  localStorage.setItem("leftCloseCol", leftImgList.innerHTML);
+};
+heartBtnFull.onclick = function () {
+  heartBtnFull.classList.add("d-none");
+  heartBtn.classList.remove("d-none");
+  const library = document.querySelectorAll(".library-card");
+  const sideImgClose = document.querySelectorAll(".side-img");
+  library.forEach((card) => {
+    const libTitle = card.querySelector(".lib-title").innerText;
+    if (libTitle === playTitle.innerText) {
+      card.remove();
+      localStorage.setItem("leftCol", playlist.innerHTML);
+    }
+  });
+  sideImgClose.forEach((img) => {
+    if (playImg.src === img.src) {
+      img.remove();
+      localStorage.setItem("leftCloseCol", leftImgList.innerHTML);
+    }
+  });
 };
 
 leftOpen.onmouseover = function () {
   libCloser.classList.remove("d-none");
+  libCloser.classList.add("slide-right");
 };
 leftOpen.onmouseout = function () {
   libCloser.classList.add("d-none");
+  libCloser.classList.remove("slide-right");
 };
 
 notify.onclick = function () {
-  right.classList.toggle("d-none");
-  if (leftOpen.classList.contains("d-none")) {
-    center.classList.toggle("col-12");
+  notiBage.classList.add("d-none");
+  if (window.innerWidth > 992) {
+    right.classList.toggle("d-none");
+    if (leftOpen.classList.contains("d-lg-block")) {
+      center.classList.toggle("col-lg-9");
+    } else {
+      center.classList.toggle("col-lg-12");
+    }
   } else {
-    center.classList.toggle("col-9");
+    if (!center.classList.contains("d-none") && !right.classList.contains("d-none")) {
+      center.classList.add("d-none");
+    } else {
+      center.classList.toggle("d-none");
+    }
   }
 };
 libCloser.onclick = function () {
-  leftOpen.classList.toggle("d-none");
+  leftOpen.classList.remove("d-lg-block");
+  leftClose.classList.remove("d-lg-none");
   if (right.classList.contains("d-none")) {
-    center.classList.toggle("col-12");
+    center.classList.toggle("col-lg-12");
   } else {
-    center.classList.toggle("col-9");
+    center.classList.toggle("col-lg-9");
   }
 };
+
+btnSide.onclick = function () {
+  leftClose.classList.add("d-lg-none");
+  leftOpen.classList.add("d-lg-block");
+  if (right.classList.contains("d-none")) {
+    center.classList.toggle("col-lg-12");
+  } else {
+    center.classList.toggle("col-lg-9");
+  }
+};
+
 srcBtn.onclick = function () {
   srcDiv.classList.toggle("bg-new");
 };
@@ -88,6 +211,7 @@ const cardGen = function (v, data) {
   cardImg.classList.add("rounded-circle");
   cardImg.style.width = "48px";
   cardText.classList.add("d-flex", "flex-column", "ms-2");
+  cardTrack.classList.add("lib-title");
   cardTrack.style.fontSize = "14px";
   cardAlbum.style.fontSize = "12px";
 
@@ -96,9 +220,26 @@ const cardGen = function (v, data) {
   cardAlbum.innerText = data.album.title;
 
   if (v) {
-    const playlist = document.getElementById("playlist");
+    const cardPlay = document.createElement("div");
+
+    cardPlay.classList.add("card-play", "d-flex", "justify-content-center", "position-absolute");
+    cardPlay.style.width = "48px";
+    cardPlay.style.height = "48px";
+    cardPlay.style.backgroundColor = " #42424282";
+    cardPlay.innerHTML = `   <svg
+                          data-encore-id="icon"
+                          role="img"
+                          aria-hidden="true"
+                          class="e-9890-icon e-9890-baseline"
+                          viewBox="0 0 24 24"
+                          style="width: 24px; fill: white"
+                        >
+                          <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+                        </svg>`;
 
     card.classList.add("library-card");
+    imgDiv.classList.add("position-relative");
+    imgDiv.appendChild(cardPlay);
     imgDiv.appendChild(cardImg);
     cardText.appendChild(cardTrack);
     cardText.appendChild(cardAlbum);
@@ -107,7 +248,6 @@ const cardGen = function (v, data) {
     card.appendChild(cardDiv);
     playlist.appendChild(card);
   } else {
-    const frends = document.getElementById("frend");
     card.classList.add("frend-card");
     cardDiv.classList.remove("align-items-center");
 
@@ -118,10 +258,11 @@ const cardGen = function (v, data) {
     cardName.style.fontSize = "14px";
     cardTrack.style.fontSize = "12px";
     timeText.style.fontSize = "10px";
+    timeText.classList.add("time");
 
     cardImg.src = data.album.cover_small;
     cardName.innerText = "sbabulli";
-    timeText.innerText = "8 min";
+    timeText.innerText = "now";
     cardTrack.innerHTML = `${data.title}<i class="bi bi-dot"></i>${data.artist.name} `;
     cardAlbum.innerHTML = `<i class="bi bi-disc"></i> ${data.album.title}`;
 
@@ -134,6 +275,76 @@ const cardGen = function (v, data) {
     cardDiv.appendChild(cardText);
     cardDiv.appendChild(cardTime);
     card.appendChild(cardDiv);
-    frends.appendChild(card);
+    frends.insertBefore(card, frends.children[0]);
   }
+};
+
+// const messageGen = function () {
+//   const friendsArry = [];
+//   const randFriendAlbum = ["eminem", "adele", "drake", "coldplay"];
+
+//   for (let i = 0; i < 3; i++) {
+//     const randNum = Math.floor(Math.random() * randFriendAlbum.length);
+//     console.log(randFriendAlbum[randNum]);
+
+//   }
+// };
+
+fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=drake", {
+  headers: {
+    "x-rapidapi-key": "d5ce525b28msh47691e1e7c71304p1ee8bajsnf3418b6f6e9b",
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+})
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
+  })
+  .then((data) => {
+    const allData = data.data;
+    console.log(allData);
+    if (frends.innerHTML === "") {
+      cardGen(false, allData[2]);
+    }
+
+    let randTime = Math.floor(Math.random() * 60000) + 20000;
+    console.log(randTime);
+
+    setInterval(() => {
+      const randTime2 = Math.floor(Math.random() * randTime);
+      console.log("rand2", randTime2);
+      setTimeout(() => {
+        const randNum = Math.floor(Math.random() * allData.length);
+        cardGen(false, allData[randNum]);
+        localStorage.setItem("rightCol", frends.innerHTML);
+        if (right.classList.contains("d-none")) {
+          notiBage.classList.remove("d-none");
+        }
+      }, randTime2);
+      randTime = Math.floor(Math.random() * 30000) + 5000;
+    }, randTime);
+    setInterval(() => {
+      const allTime = document.querySelectorAll(".time");
+      randTime = Math.floor(Math.random() * 30000) + 5000;
+      console.log(randTime);
+      allTime.forEach((time) => {
+        if (time.innerText === "now") {
+          time.innerText = "1 min";
+        } else {
+          time.innerText = `${parseInt(time.innerText) + 1} min`;
+        }
+      });
+    }, 60000);
+  })
+  .catch((error) => console.log(error));
+
+window.onload = function () {
+  const frendHtml = localStorage.getItem("rightCol");
+  const playlistCloseHtml = localStorage.getItem("leftCloseCol");
+  const playlistHtml = localStorage.getItem("leftCol");
+  frends.innerHTML = frendHtml;
+  playlist.innerHTML = playlistHtml;
+  leftImgList.innerHTML = playlistCloseHtml;
+  favoriteControl();
 };
